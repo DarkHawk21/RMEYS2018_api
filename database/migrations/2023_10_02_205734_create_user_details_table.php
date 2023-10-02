0f7e024dbc068118b09b995b73fd05a81c5237c5
Enrique Carranza <enriquecarranza-1999@live.com.mx>
2023-10-02 15:12:55 -0600

    Migrations of original tables

---
+++ database/migrations/2023_10_02_205734_create_user_details_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('identifier')
+                ->unique();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('mother_last_name');
+            $table->string('cellphone');
+            $table->integer('social_service')
+                ->default(0);
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+
+            $table->foreign('language_id')
+                ->references('id')
+                ->on('languages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
